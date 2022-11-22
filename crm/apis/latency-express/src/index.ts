113d02bce8de357cdab18c8790f3b1f6555918b2
aaronmoles <amoles@latencydata.com>
2022-11-22 13:10:22 +0100

    feat: git reset cache

--- crm/apis/latency-express/src/index.ts
+++ crm/apis/latency-express/src/index.ts
@@ -1,10 +1,8 @@
 import 'reflect-metadata';
 import {LatencyExpressApp} from "./latency-express.app";
-import {packageA} from "@latency/package-a";
 
 try {
     new LatencyExpressApp().start();
-    packageA();
 } catch (e) {
     console.log(e);
     process.exit(1);
